36b4c1941fd68e4fa403c7d4744a1b3d43c56e12
Metin Türk <metinturk553@gmail.com>
2025-10-03 15:53:01 +0300

    küçük bir atm otomasyonu yaptım.

---
+++ CalismaKodlari/BasittenZoraGidenCalismalar/BankaHesabi/BankaHesabi/Banka.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankaHesabi
+{
+    internal class Banka
+    {
+        private string _BankaHesabi;
+
+        public Banka(string bankaHesabi)
+        {
+            _BankaHesabi = bankaHesabi;
+        }
+
+        public string BankaHesabi { get => _BankaHesabi; set => _BankaHesabi = value; }
+
+        private double _bakiye = 0;
+
+        public double bakiye { get => _bakiye; set => _bakiye = value; }
+
+        public void ParaYatir()
+        {
+            int tutar;
+            do
+            {
+                Console.WriteLine($"Bakiyeniz: {bakiye}");
+                Console.Write("Yatırmak istediğiniz tutarı giriniz: ");
+                string tutarString = Console.ReadLine();
+                if (int.TryParse(tutarString, out tutar))
+                {
+                    bakiye += tutar;
+                    Console.WriteLine("Paranız yatırıldı iyi günler.");
+                    break;
+                }
+                else
+                {
+                    Console.Write("Hatalı tutar girişi lütfen tekrar deneyiniz.");
+                    Console.WriteLine("---------------------------------------------");
+                }
+            } while (true);
+        }
+
+        public void ParaCek()
+        {
+            int tutar;
+            do
+            {
+                Console.WriteLine($"Bakiyeniz: {bakiye}");
+                Console.Write("Çekmek istediğiniz tutarı giriniz: ");
+                string tutarString = Console.ReadLine();
+                if (int.TryParse(tutarString, out tutar))
+                {
+                    if (bakiye >= tutar)
+                    {
+                        bakiye -= tutar;
+                        Console.WriteLine("Para çekimi başarılı iyi günler.");
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Lütfen Bakiyeniziden büyük tutar girişi yapmayın.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Hatalı tutar girişi lütfen tekrar deneyiniz.");
+                    Console.WriteLine("---------------------------------------------");
+                }
+            } while (true);
+        }
+
+
+    }
+}
---
+++ CalismaKodlari/BasittenZoraGidenCalismalar/BankaHesabi/BankaHesabi/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace BankaHesabi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Banka otomasyonuna Hoşgeldiniz.");
+            int secim;
+            Console.Write("Lütfen hesap bilgilerinizi giriniz: ");
+            string hesapBilgisi = Console.ReadLine();
+            Banka bank = new Banka(hesapBilgisi);
+            string kontrol;
+            do
+            {
+                
+                do
+                {
+                    Console.Write("1-) Para Yatır\n2-) Para Çek\nLütfen yapacağınız işlemi seçiniz: ");
+                    var secimString = Console.ReadLine();
+                    if (int.TryParse(secimString, out secim))
+                    {
+                        if(secim > 0 && secim < 3)
+                        {
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Hatalı tuşlama yaptınız.");
+                    }
+                } while (true);
+                if(secim == 1)
+                {
+                    bank.ParaYatir();
+                }
+                else if(secim == 2)
+                {
+                    bank.ParaCek();
+                }
+                Console.Write("Başka bir işlem yapmak istiyor musunuz (e/h) ? ");
+                kontrol = Console.ReadLine().ToUpper();
+            } while (kontrol == "E");
+
+            
+
+        }
+    }
+}
