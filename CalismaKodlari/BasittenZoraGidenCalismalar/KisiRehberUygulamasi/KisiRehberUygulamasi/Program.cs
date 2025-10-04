f5d3631f25ec30b6df10b4291c01b7e859acf11c
Metin Türk <metinturk553@gmail.com>
2025-10-04 23:56:34 +0300

    kisi rehber otomasyonu dosyalarını oluşturdum.

---
+++ CalismaKodlari/BasittenZoraGidenCalismalar/KisiRehberUygulamasi/KisiRehberUygulamasi/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace KisiRehberUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
