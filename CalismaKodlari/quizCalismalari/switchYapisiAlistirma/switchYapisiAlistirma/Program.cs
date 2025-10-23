f6939c1e38a857fa6429f06ae642c71e3432c759
Metin Türk <metinturk553@gmail.com>
2025-10-23 21:11:38 +0300

    quiz için ufak bir çalışma yaptım.

---
+++ CalismaKodlari/quizCalismalari/switchYapisiAlistirma/switchYapisiAlistirma/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace switchYapisiAlistirma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write($"Kargonuzun agirligini giriniz: ");
+            //int agirlik = int.Parse(Console.ReadLine());
+            //Console.Write($"Kargonuzu göndereceğiniz bolgeyi seciniz (A/B/C): ");
+            //char bolgeKodu = char.Parse(Console.ReadLine());
+
+            //int kargoUcreti = (agirlik,bolgeKodu) switch
+            //{
+            //    (>= 0 and < 5, 'A') => kargoUcreti = 25,
+            //    (>=5 , 'A') => kargoUcreti = 40,
+            //    (>= 0 and <10 , 'B') => kargoUcreti = 35,
+            //    (>= 10 and <20 , 'B') => kargoUcreti = 55,
+            //    (>= 20 , 'B') => kargoUcreti = 75,
+            //    (>= 0 , 'C') => kargoUcreti = 100,
+            //    _ => kargoUcreti = 0
+            //};
+            //Console.WriteLine($"Kargo Ücretiniz: {kargoUcreti}");
+
+            for (int i = 1; i < 5; i++)
+            {
+                for (int j = 1; j < 11; j++)
+                {
+                    if(j == 3)
+                    {
+                        Console.WriteLine("Özel denetim");
+                        continue;
+                    }
+                    if(i > 2 && j > 7)
+                    {
+                        Console.WriteLine("Arızalı ürün");
+                        break;
+                    }
+                    Console.WriteLine($"{i}");
+                }
+            }
+        }
+    }
+}
