3cd2b252040e7dbdc68a99e90b3854aba93c667e
Metin Türk <metinturk553@gmail.com>
2025-11-05 18:30:33 +0300

    vize öncesi son alistirmalar.

---
+++ CalismaKodlari/VizeCalisma/DuzensizDiziOrnegi/DuzensizDiziOrnegi/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace DuzensizDiziOrnegi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string hamVeri = "CNTR:101-Urun:5_Risk:2_ACIL , CNTR:102-Urun:3_Risk:1_YAVAŞ | CNTR:204-Urun:4_Risk:0_NORMAL | CNTR:307-Urun:6_Risk:2_KRITIK";
+            string[] konteynerlar = hamVeri.Split('|');
+            int[][] riskDurumlari = new int[3][];
+            string[] urunler;
+            for (int i = 0; i < konteynerlar.Length; i++)
+            {
+                urunler = konteynerlar[i].ToString().Split(",");
+                riskDurumlari[i] = new int[urunler.Length];
+                string[] duzenliVeri = urunler[i].Split("_");
+                int riskKodu = int.Parse(duzenliVeri[1].Substring(duzenliVeri[1].Length - 1 , 1).ToString());
+                for (int j = 0; j < urunler.Length; j++)
+                {
+                    
+                }
+
+            }
+        }
+    }
+}
