6579effbd791ef81267707da881d0db2e650a116
Metin Türk <metinturk553@gmail.com>
2026-01-03 22:52:30 +0300

    OOP_Tekrari yaptım.

---
+++ CalismaKodlari/finalCalisma/OOP_Tekrari/OOP_Tekrari/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_Tekrari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
